a47f082f6d43c8e892702bbbb4ccd96e1a9c1c53
i-am-arav <aravindkumartechtrek@gmail.com>
2022-10-07 23:55:48 +0530

    Uber Clone day-2

--- pages/components/map.js
+++ pages/components/map.js
@@ -4,16 +4,34 @@ import tw from "tailwind-styled-components"
 import mapboxgl from 'mapbox-gl'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiYXJhdmluZC1hayIsImEiOiJjbDhuZW5qMWgwczExM3Zxd3Znc3ZkaDYzIn0.gj9rfK_f9bS5LIJsajJfrQ'
-export default function Map() {
+export default function Map({pickupCoordinates,dropCoordinates}) {
 
   useEffect(() => {
     const map = new mapboxgl.Map({
     container:'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center:[78.157982,11.652120],
-    zoom:12
+    zoom:8
   })
-  },[])
+  if(pickupCoordinates) {
+    addToMap(map,pickupCoordinates);
+  }
+  if(dropCoordinates) {
+    addToMap(map,dropCoordinates);
+  }
+  if(pickupCoordinates && dropCoordinates) {
+    map.fitBounds([pickupCoordinates,dropCoordinates], {
+      padding:60
+    })
+  }
+  
+  
+ 
+  },[pickupCoordinates,dropCoordinates]);
+
+  const addToMap = (map,coordinates) => {
+     const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map); 
+  }
 
   return (
 
--- pages/confirm.js
+++ pages/confirm.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import tw from 'tailwind-styled-components';
 import Map from './components/map';
 export default function confirm() {
+
+  const [pickup,setPickup] = useState();
+  const [drop,setDrop] = useState();
+
+  const getDropCoordinates = () => {
+    const dropLocation = 'Namakkal';
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${dropLocation}.json?` +
+    new URLSearchParams({
+      access_token : "pk.eyJ1IjoiYXJhdmluZC1hayIsImEiOiJjbDhuZW5qMWgwczExM3Zxd3Znc3ZkaDYzIn0.gj9rfK_f9bS5LIJsajJfrQ",
+      limit:1
+    })
+    )
+      .then(res => res.json())
+      .then((data) => {
+        setDrop(data.features[0].center)
+      }); 
+  }
+  const getPickupCoordinates = () => {
+    const pickupLocation = 'Salem,TamilNadu';
+    fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${pickupLocation}.json?` +
+    new URLSearchParams({
+      access_token : "pk.eyJ1IjoiYXJhdmluZC1hayIsImEiOiJjbDhuZW5qMWgwczExM3Zxd3Znc3ZkaDYzIn0.gj9rfK_f9bS5LIJsajJfrQ",
+      limit:1
+    })
+    )
+      .then(res => res.json())
+      .then(data => {setPickup(data.features[0].center)});
+  }
+  useEffect(() => {
+    getDropCoordinates();
+    getPickupCoordinates();
+  }, [])
   return (
     <Wrapper>
-        <MapContainer></MapContainer>
+        <Map
+        pickupCoordinates={pickup}
+        dropCoordinates={drop}
+        />
         <RideContainer>
             <InfoText>Choose a ride or swipe up for more</InfoText>
         </RideContainer>
